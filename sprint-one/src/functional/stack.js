da4ee61f0f8af9ec7fd737d0dbdec272d341da5a
Matthew Vargeson <matthew.vargeson@inovia-team.com>
2016-04-20 12:05:50 -0700

    add functional inst stack impl

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -6,12 +6,18 @@ var Stack = function() {
 
   // Implement the methods below
   someInstance.push = function(value) {
+    storage[someInstance.size()] = value;
   };
 
   someInstance.pop = function() {
+    var i = someInstance.size() - 1;
+    var value = storage[i];
+    delete storage[i];
+    return value;
   };
 
   someInstance.size = function() {
+    return Object.keys(storage).length;
   };
 
   return someInstance;
