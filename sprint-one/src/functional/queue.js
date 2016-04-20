19df3fd4cf6c828c53256b56a76681e6ffa4bb5c
Matthew Vargeson <matthew.vargeson@inovia-team.com>
2016-04-20 12:22:27 -0700

    add the functional inst of a queue

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -7,12 +7,27 @@ var Queue = function() {
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    storage[someInstance.size()] = value;
   };
 
   someInstance.dequeue = function() {
+    var first = Object.keys(storage)[0];
+    var value = storage[first];
+
+    // clean up the storage by removing the first item
+    delete storage[first];
+
+    // then shift the items behind it
+    for (var key in storage) {
+      storage[key - 1] = storage[key];
+      delete storage[key];
+    }
+
+    return value;
   };
 
   someInstance.size = function() {
+    return Object.keys(storage).length;
   };
 
   return someInstance;
