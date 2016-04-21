684fe5154e9520f2004817cfcb184d27ab43832a
ashjd <ashjd29@gmail.com>
2016-04-21 11:05:55 -0700

    completed linked list

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,35 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+    var newNode = Node(value);
+
+    if (list.tail !== null) {
+      list.tail.next = newNode;
+    }
+
+    list.tail = newNode;
+
+    if (list.head === null) {
+      list.head = newNode;
+    }
   };
 
   list.removeHead = function() {
+    var value = list.head.value;
+    list.head = list.head.next;
+    return value;
   };
 
   list.contains = function(target) {
+    var node = list.head;
+    while (node !== null) {
+      if (node.value === target) {
+        return true;
+      }
+      node = node.next;
+    }
+
+    return false;
   };
 
   return list;
